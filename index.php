424446ead9d9600e8c89d53918a64cb558078f26
Luke Kraakman <luke.kraakman@student.uva.nl>
2022-01-18 14:06:53 +0000

    Test Luke uploaden

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Webtechnologie voor KI/INF 2022</title>
+</head>
+
+<body>
+  <p>Als het goed is werkt de systemd service nu.</p>
+  <a href="index.php"> Home :)<a>
+</body>
+
+</html>
