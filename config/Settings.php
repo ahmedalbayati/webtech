0f548ee7cb1635c432e10f848bc6351dda43385b
Ahmed <ahmed@bayati.dev>
2022-01-20 01:15:22 +0100

    add a file with global settings

---
+++ config/Settings.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Global settings for the webapp.
+ */
+class Settings
+{
+    /**
+     * Database host.
+     *
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name.
+     *
+     * @var string
+     */
+    const DB_NAME = 'forum';
+
+    /**
+     * Database user.
+     *
+     * @var string
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database password.
+     *
+     * @var string
+     */
+    const DB_PASS = '';
+}
