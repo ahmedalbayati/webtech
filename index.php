a87c4d440ca8929865d04a383269e922b3c99a3d
Ahmed <ahmed@bayati.dev>
2022-01-22 20:42:37 +0100

    remove controller autoloading

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
-// automatically include the classes and controllers
+// automatically include the classes and models
 spl_autoload_register(function ($file_name) {
     $class_path = __DIR__ . '/classes/' . $file_name . '.php';
-    $controller_path = __DIR__ . '/controllers/' . $file_name . '.php';
+    $model_path = __DIR__ . '/models/' . $file_name . '.php';
 
     if (file_exists($class_path)) {
         include_once $class_path;
     }
-    if (file_exists($controller_path)) {
-        include_once $controller_path;
+    if (file_exists($model_path)) {
+        include_once $model_path;
     }
 });
 
