8bdd4fe4efda329b5494172da26b967bd85d3207
Ahmed <ahmed@bayati.dev>
2022-01-23 23:06:28 +0100

    add skeleton approve message file for admin dashboard

--- views/admin/approve_messages.php
+++ views/admin/approve_messages.php
@@ -1,1 +1,7 @@
+<?php $unapproved_messages = MessageModel::allMessages() ?>
+
 <h2>Approve Messages</h2>
+
+<?php foreach ($unapproved_messages as $message) { ?>
+    <div><?php echo $message['content']; ?></div>
+<?php } ?>
