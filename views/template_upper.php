3a0266bf4b0ecfdf32dff8b0946e70cad9236a4a
Ahmed <ahmed@bayati.dev>
2022-01-23 18:11:23 +0100

    fix the names of the global css and javascript files

--- views/template_upper.php
+++ views/template_upper.php
@@ -7,8 +7,8 @@
         <meta name="author" content="Ahmed, Zhi, Luke, Dante & Joris">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="../static/styles.css">
-        <script type="text/javascript" src="../static/script.js"></script>
+        <link rel="stylesheet" href="../static/global.css">
+        <script type="text/javascript" src="../static/global.js"></script>
 
         <!-- fontawesome -->
         <script src="https://kit.fontawesome.com/7b278d3198.js" crossorigin="anonymous"></script>
