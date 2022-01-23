2f40d2b95d2bc8210930f0b627f7ac0654a4fd3c
Ahmed <ahmed@bayati.dev>
2022-01-23 18:09:59 +0100

    add an ajax function that switches between pages for the dashboard

--- static/global.js
+++ static/global.js
@@ -5,3 +5,14 @@ function toggleDropdown() {
         document.getElementsByClassName('nav-dropdown')[0].style.display = 'none';
     }
 }
+
+function ajaxSwitchPage(request, div_id) {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', request);
+    xhttp.send();
+
+    xhttp.onload = function() {
+        document.getElementById(div_id).innerHTML = xhttp.responseText;
+    }
+}
