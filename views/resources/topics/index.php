ab3daed670e3cf20baf573d95917061781a434dc
Ahmed <ahmed@bayati.dev>
2022-01-23 22:29:44 +0100

    make css class name more logical

--- views/resources/topics/index.php
+++ views/resources/topics/index.php
@@ -5,15 +5,13 @@
 <div style="margin-left: 30%;margin-right:30%;">
     <div style="text-align: center;">
         <h2>First try looking up your question.</h2>
-        <input type="text">
+        <input type="text" style="width: 667px; height: 48px;">
     </div>
 
     <h2>Otherwise, create a new question.</h2>
     <?php foreach ($topics as $topic) { ?>
         <a href="<?php echo '/topic/' . $topic['id'] ?>">
-            <div class="topic-card">
-                <?php echo $topic['name']; ?>
-            </div>
+            <div class="index-topic-card"><?php echo $topic['name']; ?></div>
         </a>
     <?php } ?>
 </div>
