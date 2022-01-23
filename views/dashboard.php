5d80a6a9454115d9c8bac396b571fc0834decd29
Ahmed <ahmed@bayati.dev>
2022-01-23 18:09:05 +0100

    add a basic dashboard page with a vertical nav that uses ajax to switch between pages

---
+++ views/dashboard.php
@@ -0,0 +1,15 @@
+<h1>Dashboard</h1>
+
+<ul class="nav-vertical">
+    <li class="nav-vertical-item">
+        <a href="#" onclick="ajaxSwitchPage('edit-topics', 'page');">edit topics</a>
+    </li>
+    <li class="nav-vertical-item">
+        <a href="#" onclick="ajaxSwitchPage('approve-messages', 'page');">approve messages</a>
+    </li>
+    <li class="nav-vertical-item">
+        <a href="#" onclick="ajaxSwitchPage('forum-settings', 'page');"">forum settings</a>
+    </li>
+</ul>
+
+<div id="page"></div>
